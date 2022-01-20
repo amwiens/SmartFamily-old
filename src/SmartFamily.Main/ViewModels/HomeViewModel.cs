91a24ca3005b4e90c14304e960fdb65a28766833
Aaron Wiens <aaron@awwebsolutions.com>
2022-01-19 20:41:16 -0600

    Have open recent file from the recent files list working on click.

--- src/SmartFamily.Main/ViewModels/HomeViewModel.cs
+++ src/SmartFamily.Main/ViewModels/HomeViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.Extensions.Logging;
 
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using Prism.Services.Dialogs;
 
 using SmartFamily.Core;
+using SmartFamily.Core.Constants;
+using SmartFamily.Core.Contracts.Services;
+using SmartFamily.Core.Exceptions;
 using SmartFamily.Core.Models;
+using SmartFamily.Core.WPF.Contracts.Services;
+using SmartFamily.Core.WPF.Dialogs;
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Windows.Input;
 
 namespace SmartFamily.Main.ViewModels
 {
@@ -17,10 +27,15 @@ namespace SmartFamily.Main.ViewModels
     public class HomeViewModel : BindableBase, INavigationAware
     {
         private readonly IRegionManager _regionManager;
+        private readonly IOpenFileDialogService _openFileDialogService;
+        private readonly IDatabaseService _databaseService;
+        private readonly IDialogService _dialogService;
         private readonly ILogger<HomeViewModel> _logger;
 
         private IRegionNavigationService? _navigationService;
         private List<RecentFile> _recentFiles;
+
+        private ICommand? _menuFileOpenCommand;
 
         /// <summary>
         /// Gets the recent files.
@@ -35,17 +50,57 @@ namespace SmartFamily.Main.ViewModels
         }
 
         /// <summary>
+        /// File -> open menu command.
+        /// </summary>
+        public ICommand MenuFileOpenCommand => _menuFileOpenCommand ?? (_menuFileOpenCommand = new DelegateCommand<RecentFile>(OnMenuFileOpen));
+
+        /// <summary>
         /// Ctor
         /// </summary>
         /// <param name="regionManager">Region manager.</param>
         /// <param name="logger">Logger.</param>
         public HomeViewModel(IRegionManager regionManager,
+            IOpenFileDialogService openFileDialogService,
+            IDatabaseService databaseService,
+            IDialogService dialogService,
             ILogger<HomeViewModel> logger)
         {
             _regionManager = regionManager;
+            _openFileDialogService = openFileDialogService;
+            _databaseService = databaseService;
+            _dialogService = dialogService;
             _logger = logger;
 
             GetRecentFiles();
+        }
+
+        /// <summary>
+        /// Request navigate.
+        /// </summary>
+        /// <param name="target">Target page.</param>
+        /// <returns><c>true</c> if page can be navigated to, otherwise <c>false</c>.</returns>
+        private bool RequestNavigate(string target)
+        {
+            if (_navigationService.CanNavigate(target))
+            {
+                _navigationService.RequestNavigate(target);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Navigate to the target page.
+        /// </summary>
+        /// <param name="target">Target page.</param>
+        private void RequestNavigateAndCleanJournal(string target)
+        {
+            var navigated = RequestNavigate(target);
+            if (navigated)
+            {
+                _navigationService.Journal.Clear();
+            }
         }
 
         /// <summary>
@@ -71,6 +126,8 @@ namespace SmartFamily.Main.ViewModels
         /// <param name="navigationContext">Navigation context.</param>
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+            _navigationService = _regionManager.Regions[Regions.Main].NavigationService;
+            _navigationService.Navigated += OnNavigated;
             _logger.LogInformation("HomeViewModel: Navigated to.");
 
             GetRecentFiles();
@@ -91,6 +148,64 @@ namespace SmartFamily.Main.ViewModels
         private void GetRecentFiles()
         {
             RecentFiles = ApplicationSettings.RecentFiles.OrderByDescending(x => x.LastOpened).Take(10).ToList();
+        }
+
+        /// <summary>
+        /// Open a file.
+        /// </summary>
+        private void OnMenuFileOpen(RecentFile recentFile)
+        {
+            var fileName = Path.Combine(recentFile.FilePath, recentFile.FileName);
+            if (File.Exists(fileName))
+                OpenDatabaseFile(fileName);
+        }
+
+        /// <summary>
+        /// Opens the database file.
+        /// </summary>
+        /// <param name="databasePath">Path to the database file.</param>
+        private void OpenDatabaseFile(string databasePath)
+        {
+            try
+            {
+                ApplicationSettings.OpenDatabase = _databaseService.OpenDatabase(databasePath);
+
+                if (ApplicationSettings.RecentFiles is null)
+                {
+                    ApplicationSettings.RecentFiles = new List<RecentFile>();
+                }
+                var recentFile = new RecentFile
+                {
+                    FileName = databasePath.Substring(databasePath.LastIndexOf('\\') + 1),
+                    FilePath = databasePath.Substring(0, databasePath.LastIndexOf('\\')),
+                    LastOpened = DateTime.Now,
+                };
+
+                var file = ApplicationSettings.RecentFiles.Where(x => x.FileName == recentFile.FileName && x.FilePath == recentFile.FilePath);
+
+                if (file.Any())
+                {
+                    file.FirstOrDefault().LastOpened = DateTime.Now;
+                }
+                else
+                {
+                    ApplicationSettings.RecentFiles.Add(recentFile);
+                }
+
+                //CloseEnabled = true;
+                _logger.LogInformation("HomeViewModel: {databasePath} opened.", databasePath);
+                RequestNavigateAndCleanJournal(PageKeys.Main);
+            }
+            catch (DatabaseFormatException ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                _dialogService.ShowNotification(ex.Message, r => { });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                _dialogService.ShowNotification(ex.Message, r => { });
+            }
         }
     }
 }
