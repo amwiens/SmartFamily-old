64ff19f39a514ea38c91cde32d9e0242d65c1b80
Aaron Wiens <aaron@awwebsolutions.com>
2022-01-27 22:13:25 -0600

    Working on more guards.

---
+++ src/SmartFamily.Core/Guards/Guard.Modify.cs
@@ -0,0 +1,112 @@
+﻿using JetBrains.Annotations;
+
+using System.Diagnostics;
+
+namespace SmartFamily.Core.Guards
+{
+    /// <content>
+    /// Provides safe modification functions to normalize arguments.
+    /// </content>
+    public static partial class Guard
+    {
+        /// <summary>
+        /// Returns a new argument with the same name and the specified value.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the existing argument.</typeparam>
+        /// <typeparam name="TTarget">The type of the new argument.</typeparam>
+        /// <param name="argument">The existing argument.</param>
+        /// <param name="value">The new argument value.</param>
+        /// <returns>A new <see cref="ArgumentInfo{T}"/>.</returns>
+        [DebuggerStepThrough]
+        [GuardFunction("Normalization", "gmod")]
+        public static ArgumentInfo<TTarget> Modify<TSource, TTarget>(
+            in this ArgumentInfo<TSource> argument, TTarget value)
+            => new ArgumentInfo<TTarget>(value, argument.Name, true, argument.Secure);
+
+        /// <summary>
+        /// Returns a new argument with the same name and a value that is created using the
+        /// specified conversion function.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the existing argument.</typeparam>
+        /// <typeparam name="TTarget">The type of the new argument.</typeparam>
+        /// <param name="argument">The existing argument.</param>
+        /// <param name="convert">
+        ///     A function that accepts the existing argument's value and returns a new object to be
+        ///     used as the new argument's value.
+        /// </param>
+        /// <returns>A new <see cref="ArgumentInfo{TTarget}"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="convert"/> is <c>null</c>.</exception>
+        [ContractAnnotation("convert:null => halt")]
+        [DebuggerStepThrough]
+        [GuardFunction("Normalization", "gmod")]
+        public static ArgumentInfo<TTarget> Modify<TSource, TTarget>(
+            in this ArgumentInfo<TSource> argument, Func<TSource, TTarget> convert)
+        {
+            Argument(convert, nameof(convert)).NotNull();
+            return new ArgumentInfo<TTarget>(
+                convert(argument.Value), argument.Name, true, argument.Secure);
+        }
+
+        /// <summary>
+        /// <para>
+        ///     Returns a new argument with the same name and a value that is created using the
+        ///     specified convertion function.
+        /// </para>
+        /// <para>
+        ///     If the conversion function throws an exception, it will be wrapped in an <see cref="ArgumentException"/>.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TSource">The type of the existing argument.</typeparam>
+        /// <typeparam name="TTarget">The type of the new argument.</typeparam>
+        /// <param name="argument">The existing argument.</param>
+        /// <param name="convert">
+        ///     A function that accepts the existing argument's value and returns a new object ot be
+        ///     used as the new argument's value.
+        /// </param>
+        /// <param name="message">
+        ///     The factory to initialize the message of the exception that will be thrown if the
+        ///     conversion function fails.
+        /// </param>
+        /// <returns>A new <see cref="ArgumentInfo{TTarget}"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="convert"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="convert"/> threw an exception.</exception>
+        [ContractAnnotation("convert:null => halt")]
+        [DebuggerStepThrough]
+        [GuardFunction("normalization", "gwrap")]
+        public static ArgumentInfo<TTarget> Wrap<TSource, TTarget>(
+            in this ArgumentInfo<TSource> argument,
+            Func<TSource, TTarget> convert,
+            Func<TSource, string>? message = null)
+        {
+            Argument(convert, nameof(convert)).NotNull();
+            try
+            {
+                return new ArgumentInfo<TTarget>(
+                    convert(argument.Value), argument.Name, true, argument.Secure);
+            }
+            catch (Exception ex)
+            {
+                var m = message?.Invoke(argument.Value) ?? Messages.Require(argument);
+                throw Fail(new ArgumentException(m, argument.Name, ex));
+            }
+        }
+
+        /// <summary>
+        /// Returns a new argument with the same name and a shallow clone of the original value.
+        /// </summary>
+        /// <typeparam name="T">The type of the clonable argument.</typeparam>
+        /// <param name="argument">The cloneable argument.</param>
+        /// <returns>A new <see cref="ArgumentInfo{T}"/>.</returns>
+        [DebuggerStepThrough]
+        [GuardFunction("Normalization", "gclone")]
+        public static ArgumentInfo<T> Clone<T>(in this ArgumentInfo<T> argument)
+            where T : class, ICloneable?
+        {
+            if (!argument.HasValue())
+                return argument;
+
+            var clone = argument.Value!.Clone() as T;
+            return new ArgumentInfo<T>(clone!, argument.Name, argument.Modified, argument.Secure);
+        }
+    }
+}
---
+++ src/SmartFamily.Core/Guards/Guard.Types.cs
@@ -0,0 +1,327 @@
+﻿using JetBrains.Annotations;
+
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace SmartFamily.Core.Guards
+{
+    /// <content>
+    /// Provides type preconditions.
+    /// </content>
+    public static partial class Guard
+    {
+        /// <summary>
+        /// Requires the argument to have a value that is an instance of the specified generic type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type that the argument's value should be an instance of.
+        /// </typeparam>
+        /// <param name="argument">The object argument.</param>
+        /// <param name="message">
+        ///     The factory to initialize the message of the exception that will be thrown if the
+        ///     precondition is not satisfied.
+        /// </param>
+        /// <returns>A new <see cref="ArgumentInfo{T}"/>.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument"/> value is not an instance of type <typeparamref name="T"/>.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("Type")]
+        public static ArgumentInfo<T> Type<T>(
+            in this ArgumentInfo<object> argument, Func<object, string>? message = null)
+        {
+            if (!TypeInfo<T>.CanBeInitializedFrom(argument.Value))
+            {
+                var m = message?.Invoke(argument.Value) ?? Messages.Type(argument, typeof(T));
+                throw Fail(new ArgumentException(m, argument.Name));
+            }
+
+            return new ArgumentInfo<T>(
+                (T)argument.Value, argument.Name, argument.Modified, argument.Secure);
+        }
+
+        /// <summary>
+        /// Requires the argument to have a value that is not an instance of the specified
+        /// generic type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type that the argument's value should not be an instance of.
+        /// </typeparam>
+        /// <param name="argument">The argument.</param>
+        /// <param name="message">
+        ///     The factory to initialize the message of the exception that will be thrown if the
+        ///     precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument"/>.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument"/> value is an instance of type <typeparamref name="T"/>.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("Type")]
+        public static ref readonly ArgumentInfo<object> NotType<T>(
+            in this ArgumentInfo<object> argument, Func<T, string>? message = null)
+        {
+            if (argument.HasValue() && TypeInfo<T>.CanBeInitializedFrom(argument.Value))
+            {
+                var m = message?.Invoke((T)argument.Value) ?? Messages.NotType(argument, typeof(T));
+                throw Fail(new ArgumentException(m, argument.Name));
+            }
+
+            return ref argument;
+        }
+
+        /// <summary>
+        /// Requires the argument to have a value that is an instance of the specified type.
+        /// </summary>
+        /// <param name="argument">The object argument.</param>
+        /// <param name="type">The type that the arguent's value should be an instance of.</param>
+        /// <param name="message">
+        ///     The factory to initialize the message of the exception that will be thrown if the
+        ///     precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument"/>.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument"/> value is not an instance of the type represented by <paramref name="type"/>.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("Type")]
+        public static ref readonly ArgumentInfo<object> Type(
+            in this ArgumentInfo<object> argument, Type type, Func<object, Type, string>? message = null)
+        {
+            if (argument.HasValue() && !TypeInfo.CanBeConvertedTo(argument.Value, type))
+            {
+                var m = message?.Invoke(argument.Value, type) ?? Messages.Type(argument, type);
+                throw Fail(new ArgumentException(m, argument.Name));
+            }
+
+            return ref argument;
+        }
+
+        /// <summary>
+        ///     Requires the argument to have a value that is not an instance of the specified type.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="type">
+        ///     The type that the argument's value should not be an instance of.
+        /// </param>
+        /// <param name="message">
+        ///     The factory to initialize the message of the exception that will be thrown if the
+        ///     precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument"/>.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument"/> value is an instance of the type represented by <paramref name="type"/>.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("Type")]
+        public static ref readonly ArgumentInfo<object> NotType(
+            in this ArgumentInfo<object> argument, Type type, Func<object, Type, string>? message = null)
+        {
+            if (argument.HasValue() && TypeInfo.CanBeConvertedTo(argument.Value, type))
+            {
+                var m = message?.Invoke(argument.Value, type) ?? Messages.NotType(argument, type);
+                throw Fail(new ArgumentException(m, argument.Name));
+            }
+
+            return ref argument;
+        }
+
+        /// <content>
+        /// Contains the compatibility preconditions.
+        /// </content>
+        public partial struct ArgumentInfo<T>
+        {
+            /// <summary>
+            /// Requires the argument to have a value that can be assigned to an instance of the
+            /// specified type.
+            /// </summary>
+            /// <typeparam name="TTarget">
+            ///     The type that the argument's value should be assignable to.
+            /// </typeparam>
+            /// <param name="message">
+            ///     The factory to initialize the message of the exception that will be thrown if the
+            ///     precondition is not satisfied.
+            /// </param>
+            /// <returns>The current argument.</returns>
+            /// <exception cref="ArgumentException">
+            ///     <see cref="Value"/> cannot be assigned to type <typeparamref name="TTarget"/>.
+            /// </exception>
+            [AssertionMethod]
+            [DebuggerStepThrough]
+            [GuardFunction("Type", "gcomp")]
+            public ArgumentInfo<T> Compatible<TTarget>(Func<T, string>? message = null)
+            {
+                if (!HasValue() || Value is TTarget value)
+                    return this;
+
+                var m = message?.Invoke(Value) ?? Messages.Compatible<T, TTarget>(this);
+                throw Fail(new ArgumentException(m, Name));
+            }
+
+            /// <summary>
+            /// Requires the argument to have a value that cannot be assigned to an instance of
+            /// the specified type.
+            /// </summary>
+            /// <typeparam name="TTarget">
+            ///     The type that the argument's value should not be assignable to.
+            /// </typeparam>
+            /// <param name="message">
+            ///     The factory to initialize the message of the exception that will be thrown if the
+            ///     precondition is not satisfied.
+            /// </param>
+            /// <returns>The current argument.</returns>
+            /// <exception cref="ArgumentException">
+            ///     <see cref="Value"/> can be assigned to type <typeparamref name="TTarget"/>.
+            /// </exception>
+            [AssertionMethod]
+            [DebuggerStepThrough]
+            [GuardFunction("Type", "gncomp")]
+            public ArgumentInfo<T> NotCompatible<TTarget>(Func<TTarget, string>? message = null)
+            {
+                if (HasValue() && Value is TTarget value)
+                {
+                    var m = message?.Invoke(value) ?? Messages.NotCompatible<T, TTarget>(this);
+                    throw Fail(new ArgumentException(m, Name));
+                }
+
+                return this;
+            }
+
+            /// <summary>
+            /// <para>
+            ///     Requires the argument to have a value that can be assigned to an instance of
+            ///     the specified type.
+            /// </para>
+            /// <para>The return value will be a new argument of type <typeparamref name="TTarget"/>.</para>
+            /// </summary>
+            /// <typeparam name="TTarget">
+            ///     The type that the argument's value should be assignable to.
+            /// </typeparam>
+            /// <param name="message">
+            ///     The factory to initialize the message of the exception that will be thrown if the
+            ///     precondition is not satisfied.
+            /// </param>
+            /// <returns>A new <see cref="ArgumentInfo{TTarget}"/>.</returns>
+            /// <exception cref="ArgumentException">
+            ///     <see cref="Value"/> cannot be assigned to type <typeparamref name="TTarget"/>.
+            /// </exception>
+            [AssertionMethod]
+            [DebuggerStepThrough]
+            [GuardFunction("Type", "gcast")]
+            public ArgumentInfo<TTarget> Cast<TTarget>(Func<T, string>? message = null)
+            {
+                if (Value is TTarget value)
+                    return new ArgumentInfo<TTarget>(value, this.Name, this.Modified, this.Secure);
+
+                var m = message?.Invoke(Value) ?? Messages.Compatible<T, TTarget>(this);
+                throw Fail(new ArgumentException(m, Name));
+            }
+        }
+
+        /// <summary>
+        /// Provides cached utilities for <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        private static class TypeInfo<T>
+        {
+            /// <summary>
+            /// A function that determines whether the specified object can be converted to type <typeparamref name="T"/>.
+            /// </summary>
+            public static readonly Func<object, bool> CanBeInitializedFrom = InitCanBeInitializedFrom();
+
+            /// <summary>
+            /// Initializes <see cref="CanBeInitializedFrom"/>.
+            /// </summary>
+            /// <returns>
+            ///     A function that determines whether the specified object can be converted to type <typeparamref name="T"/>.
+            /// </returns>
+            private static Func<object, bool> InitCanBeInitializedFrom()
+            {
+                var targetType = typeof(T);
+                var isValueType = IsValueType(targetType);
+                var isNullable = !isValueType || targetType.IsGenericType(typeof(Nullable<>));
+
+                Type? type = targetType;
+                var resultChain = new HashSet<Type>();
+                do
+                {
+                    resultChain.Add(type);
+                    type = type.GetBaseType();
+                }
+                while (type != null);
+                if (isValueType && isNullable)
+                    resultChain.Add(targetType.GetGenericArguments()[0]);
+
+                resultChain.TrimExcess();
+                return obj => obj is null ? isNullable : resultChain.Contains(obj.GetType());
+            }
+        }
+
+        /// <summary>
+        /// Provides non-generic, cached utilities for specified types.
+        /// </summary>
+        private static class TypeInfo
+        {
+            /// <summary>
+            /// The locker that synchronizes access to <see cref="CanBeConvertedToDict"/>.
+            /// </summary>
+            private static readonly ReaderWriterLockSlim Locker
+                = new ReaderWriterLockSlim();
+
+            /// <summary>
+            /// The functions that determine whether a specified object can be converted to the
+            /// type that the function is mapped to.
+            /// </summary>
+            private static readonly Dictionary<Type, Func<object, bool>> CanBeConvertedToDict
+                = new Dictionary<Type, Func<object, bool>>();
+
+            /// <summary>
+            /// Determines whether an object can be converted to an instance of the specified type.
+            /// </summary>
+            /// <param name="obj">The object to check.</param>
+            /// <param name="targetType">The type to check.</param>
+            /// <returns>
+            ///     <c>true</c>, if <paramref name="obj"/> can be converted to an instance of <paramref name="targetType"/>.
+            /// </returns>
+            /// <remarks>Calls <see cref="TypeInfo{TypeInfo}.CanBeInitializedFrom"/>.</remarks>
+            public static bool CanBeConvertedTo(object obj, Type targetType)
+            {
+                Func<object, bool>? func;
+
+                Locker.EnterUpgradeableReadLock();
+                try
+                {
+                    if (!CanBeConvertedToDict.TryGetValue(targetType, out func))
+                    {
+                        var t = typeof(TypeInfo<>).MakeGenericType(targetType);
+                        var f = Expression.Field(null, t.GetField(nameof(TypeInfo<object>.CanBeInitializedFrom)));
+                        var o = Expression.Parameter(typeof(object), "obj");
+                        var c = Expression.Invoke(f, o);
+                        var l = Expression.Lambda<Func<object, bool>>(c, o);
+                        func = l.Compile();
+
+                        Locker.EnterWriteLock();
+                        try
+                        {
+                            CanBeConvertedToDict[targetType] = func;
+                        }
+                        finally
+                        {
+                            Locker.ExitWriteLock();
+                        }
+                    }
+                }
+                finally
+                {
+                    Locker.ExitUpgradeableReadLock();
+                }
+
+                return func(obj);
+            }
+        }
+    }
+}
